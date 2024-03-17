52c53919c2f643ea766317eb28033e92393b13a3
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 13:27:36 +0530

    16th course code

---
+++ Codechef/course/LBCL05_problems_CLB026.c
@@ -0,0 +1,20 @@
+// Task
+// Write a program which does the following:
+
+// Find the area of a circle whose radius is 8.9. Take pi = 3.14
+// Declare variables radius, pi and area and assign the relevant values to them
+// Formula for area of a circle is Pi * radius * radius
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+
+  double pi = 3.14;
+  double radius = 8.9;
+  double area = pi * radius * radius;
+
+  printf("The Area of the given Circle is %f", area);
+  
+}
