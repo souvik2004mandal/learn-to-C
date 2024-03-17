6fd3736d80964c2c99759a3d2d424e3505100bb9
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 12:09:47 +0530

    8th course code

---
+++ Codechef/course/LBCL04_problems_CLB017.c
@@ -0,0 +1,15 @@
+// Multiplication
+// Write a program which prints these on two separate lines.
+
+// Result of multiplication of 8 and 9
+// Result of (100 + 200) * 2
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+	printf("%d\n", 8 * 9);
+	printf("%d\n", (100 + 200) * 2);
+}
+
