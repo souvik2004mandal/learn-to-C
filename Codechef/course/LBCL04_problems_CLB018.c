5261ece7a1e18e92234b85391e07e60c6af9c292
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 12:14:12 +0530

    9th course code

---
+++ Codechef/course/LBCL04_problems_CLB018.c
@@ -0,0 +1,19 @@
+// Task
+// Write a program which does the following
+
+// Print the result of subtraction of 9 and 2
+// Print the result of division of 30 by 5
+// Output both results on separate lines
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+	// Print the result of 9 - 2
+	printf("%d\n" , 9 - 2);
+	
+	// Print the result of 30 / 5
+	printf("%d\n", 30 / 5);
+	
+}
