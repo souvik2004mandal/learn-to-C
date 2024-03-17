fa3a29e7a26ead23c1e60391021b07593a8e92a5
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 13:47:53 +0530

    19th course code

---
+++ Codechef/course/LBCL08_problems_CLB041.c
@@ -0,0 +1,17 @@
+// Task
+// Write a program which does the following
+
+// Create a variable txt and assign it the text "NumeroTres"
+// Use the strlen() function to output to the console the number of characters in txt
+
+//ANS
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char txt[] = "NumeroTres";
+    printf("The length of the word is: %d", strlen(txt));
+
+}
