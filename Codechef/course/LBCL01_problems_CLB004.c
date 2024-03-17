122517739650e540987fbde886263c8cc4d3903d
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 11:27:06 +0530

    2nd course problem

---
+++ Codechef/course/LBCL01_problems_CLB004.c
@@ -0,0 +1,38 @@
+// Arithmetic Operations
+// We can also perform mathematical operations (like addition, subtraction etc) with printf().
+
+// These use the familiar symbols of:
+
+// +
+// + for addition
+// −
+// − for subtraction
+// ∗
+// ∗ for multiplication
+// /
+// / for division
+// To add two number we do this:
+
+// printf("%d", 21 + 40);
+// The above line of code will output: 61
+
+// Again, printf is the function we use when we want to print. "%d" tells the compiler that we want to print a number. And writing 21 + 40 tells the compiler to add these two number and print the sum.
+
+// Task
+// Try to add 21 and 40 in code and print the result. Remember, we don't need "" (double quotes) when printing numbers.
+
+// Comments
+// Notice that on the editor on the right, the instructions appear in gray color.
+// This is because they are comments and don't affect the code.
+// The way we write comments is by adding "//" at the start of a line.
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+
+  // Replace the underscores
+      printf("%d" , 21 + 40);
+      return 0;
+}
---
+++ Codechef/practice/PCL01_problems_START01.c
@@ -0,0 +1,15 @@
+// Number Mirror
+// Write a program that takes a number N as the input, and prints it to the output.
+
+//ANS 
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int a;
+    scanf("%d" , &a);
+    printf("%d" , a);
+    return 0;
+}
+
