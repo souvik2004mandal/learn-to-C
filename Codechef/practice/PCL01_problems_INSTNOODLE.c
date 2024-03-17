79a545baec53ec18a2e9282e4793727be5b928cc
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 21:06:19 +0530

    3rd practice code

---
+++ Codechef/practice/PCL01_problems_INSTNOODLE.c
@@ -0,0 +1,41 @@
+// Chef and Instant Noodles
+// Chef has invented 
+// 1
+// 1-minute Instant Noodles. As the name suggests, each packet takes exactly 
+// 1
+// 1 minute to cook.
+
+// Chef's restaurant has 
+// X stoves and only 
+// 1
+// 1 packet can be cooked in a single stove at any minute.
+
+// How many customers can Chef serve in 
+// Y minutes if each customer orders exactly 
+// 1
+// 1 packet of noodles?
+
+// Input Format
+// The first and only line of input contains two space-separated integers X and Y — the number of stoves and the number of minutes, respectively.
+// Output Format
+// Print a single integer, the maximum number of customers Chef can serve in 
+// Y minutes
+// Constraints
+// 1 ≤ , ≤
+// 1000
+// 1≤X,Y≤1000
+
+
+//ANS
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b,c;
+    scanf("%d %d", &a, &b);
+    c = a * b;
+    printf("%d", c);
+    return 0;
+}
+
