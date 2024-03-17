e15d01885dfb63dc601c4e94d729d994faabeba0
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 13:52:15 +0530

    20th course code

---
+++ Codechef/course/LBCL08_problems_CLB042.c
@@ -0,0 +1,17 @@
+// Task
+// Write a program which does the following
+
+// Create a string variable word and assign the text "Programming" to it
+// Print the characters o and r from the string word in separate lines using the syntax defined above
+
+//ANS 
+
+#include <stdio.h>
+
+int main() {
+
+    char word[] = "Programming";
+    printf("%c\n", word[ 2 ]);
+    printf("%c\n", word[ 1 ]);
+    return 0;
+}
