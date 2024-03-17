5b6aad059ae35f5cfac12b186990f196c996d782
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 13:32:47 +0530

    16th course code

---
+++ Codechef/course/LBCL08_problems_CLB038.c
@@ -0,0 +1,16 @@
+// Task
+// Write a program which does the following
+
+// Create a string named fruit and assign the value "Apple" to it.
+// Output fruit to the console.
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+
+    char fruit[] = "Apple";
+    printf("%s", fruit);
+
+}
