cbd7b8f20282a312b70dddc170323490f6a2ceaa
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 20:45:21 +0530

    2nd practice code

---
+++ Codechef/practice/PCL01_problems_CPRACREARR1.c
@@ -0,0 +1,16 @@
+// Rearrange Code
+// You want to input two integers, and output their sum.
+
+// Rearrange the code given, so that it does the above task.
+
+//ANS
+
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    c = a + b;
+    printf("%d", c);
+    return 0;
+}
