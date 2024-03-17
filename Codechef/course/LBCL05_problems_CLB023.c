f8d5daa9e47fa43b89c67edcc8490ee069c87b7d
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-17 12:46:03 +0530

    11th course code

---
+++ Codechef/course/LBCL05_problems_CLB023.c
@@ -0,0 +1,19 @@
+// Task
+// Write a program which does the following
+
+// Create an integer variable named number
+// Assign the value 19 to number
+// Output the value of number.
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+    // Declare a variable
+    int number = 19;
+    
+    // Print the variable
+    printf("%d", number);
+  
+}
