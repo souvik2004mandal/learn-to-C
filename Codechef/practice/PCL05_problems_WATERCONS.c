390e514a26b7c1b4880ad42f41ff6c5b7ea73866
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-19 00:23:09 +0530

    9th practice code

---
+++ Codechef/practice/PCL05_problems_WATERCONS.c
@@ -0,0 +1,41 @@
+// Water Consumption
+// Recently, Chef visited his doctor. The doctor advised Chef to drink at least 2000 ml of water each day.
+
+// Chef drank X ml of water today. Determine if Chef followed the doctor's advice or not.
+
+// Input Format
+// The first line contains a single integer  T — the number of test cases. Then the test cases follow.
+// The first and only line of each test case contains one integer X — the amount of water Chef drank today.
+// Output Format
+// For each test case, output YES if Chef followed the doctor's advice of drinking at least 
+// 2000
+// 2000 ml of water. Otherwise, output NO.
+
+// You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+
+// Constraints
+// 1≤T≤2000
+// 1≤X≤4000
+
+//ANS
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    // Loop for each test case
+    while (t--) {
+        int x;
+        scanf("%d", &x);
+
+        // Your code for each test case goes here
+        if (x >= 2000){
+            printf("YES\n");
+        }else {
+            printf("NO\n");
+        }
+    }
+
+}
