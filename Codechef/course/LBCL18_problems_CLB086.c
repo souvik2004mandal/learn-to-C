1e7750fa5155518c88b14a34dca99ebc8c3d82c7
SOUVIK MANDAL <147265809+souvik2004mandal@users.noreply.github.com>
2024-03-21 19:26:52 +0530

    31st course code

---
+++ Codechef/course/LBCL18_problems_CLB086.c
@@ -0,0 +1,27 @@
+// Creating Arrays
+// Arrays are used to store multiple items of the same type together.
+
+// An array is defined by specifying the type of its elements and the number of elements it will hold. For example, int numbers[5]; declares an array numbers that can hold five integers.
+
+// You can set values at the time of declaration, like int numbers[5] = {1, 2, 3, 4, 5};. Here, each element in the array numbers gets the corresponding value.
+
+// Example:
+
+// int numbers[5] = {1, 2, 3, 4, 5};
+// Task
+// Write a program which does the following
+
+// Create an Array of the first 5 positive integers
+// Once the array is define - output "Done" to the console.
+
+//ANS
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int number[5] = {1, 2, 3, 4, 5};
+    printf("Done");
+    return 0;
+}
+
